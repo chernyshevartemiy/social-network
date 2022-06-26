62169fd0424fd7022481957d53989e7069d91133
Artem Chernyshev <artem.octopus@mail.ru>
2022-06-26 14:26:06 +0300

    links: created navlinks

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,9 +6,9 @@ const Nav = () => {
     <ul className={s.Nav__list}>
         <li className={s.Item}><NavLink to="/Profile" className = {({isActive}) => isActive ? s.active : s.Item }>Profile</NavLink></li>
         <li className={s.Item}><NavLink to="/Dialogs" className = {({isActive})=> isActive ? s.active : s.Item }>Messages</NavLink></li>
-        <li className={s.Item}><a href="">News</a></li>
-        <li className={s.Item}><a href="">Music</a></li>
-        <li className={s.Item}><a href="">Settings</a></li>
+        <li className={s.Item}><NavLink to="/News" className = {({isActive})=> isActive ? s.active : s.Item }>News</NavLink></li>
+        <li className={s.Item}><NavLink to="/Music" className = {({isActive})=> isActive ? s.active : s.Item }>Music</NavLink></li>
+        <li className={s.Item}><NavLink to="/Settings" className = {({isActive})=> isActive ? s.active : s.Item }>Settings</NavLink></li>
     </ul>
 </nav> )
 }

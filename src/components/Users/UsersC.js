d631312b24f4284a74caca11dfd523c2daba0f84
Artem Chernyshev <artem.octopus@mail.ru>
2022-07-10 00:02:11 +0300

    u: small mistakes were fixed

--- src/components/Users/UsersC.js
+++ src/components/Users/UsersC.js
@@ -5,9 +5,8 @@ import React from 'react'
 
 class UserC extends React.Component {
 
-	
 	componentDidMount() {
-		axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
 		this.props.setUsers(response.data.items)
 		})}
 
@@ -15,7 +14,7 @@ class UserC extends React.Component {
 		super(props);
 		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
 			this.props.setUsers(response.data.items)
-			this.props.setTotalUsersCount(response.data.totalCount)
+			this.props.setTotalUsersCount(response.data.totalCount) 
 		});
 	}
 	onPageChanged = (pageNumber) => {

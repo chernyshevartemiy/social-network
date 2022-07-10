14efd4ccb64faa7fa6261e298c8f1fc3971961e7
Artem Chernyshev <artem.octopus@mail.ru>
2022-07-10 14:33:37 +0300

    u: users component was modified & UsersC was deleted

---
+++ src/components/Users/Users.js
@@ -0,0 +1,41 @@
+import React from "react";
+import s from './Users.module.css';
+import userImg from '../../img/profile.png';
+
+let Users = (props) => {
+	let pagesCount = Math.ceil(
+		props.totalUsersCount / props.pageSize
+	);
+
+	let pages = [];
+	for (let i = 1; i <= pagesCount; i++) {
+		pages.push(i);
+	}
+	return (
+		<div>
+			{props.users.map((e) => {
+				return <div className = {s.User} key = {`${e.id}_${e.name}`}>
+							<div className = {s.User__profile}>
+								<img className = {s.User__img} src={e?.photos?.small || userImg} alt="#"/>
+								{e.followed ? <button className = {s.User__button} onClick = {() => { props.unfollow(e.id) }}>Unfollow</button> : <button className = {s.User__button} onClick = {() => { props.follow(e.id) }}>Follow</button>}
+							</div>
+							<div className = {s.User__info}>
+								<div className = {s.User__initials}>
+									<div className = {s.User__name}>Name: {e.name}</div>
+									<div className = {s.User_description}>Status: {e.status}</div>
+								</div>
+								<div className = {s.User__area}>
+									<div className = {s.User__country}>Country: {"e.location.country"}</div>
+									<div className = {s.User__city}>City: {"e.location.city"}</div>
+								</div>
+							</div>
+					   </div>
+			})}
+					   {pages.map(p => {
+						   return <span className = {props.currentPage === p && s.selectedPage} onClick = {() => {props.onPageChanged(p)}}>{p}</span>
+					   })}	   
+		</div>
+	)
+};
+
+export default Users

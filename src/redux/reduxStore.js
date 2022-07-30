8cb24a530216ea2f575081c86553659b0c292019
Artem Chernyshev <artem.octopus@mail.ru>
2022-07-30 13:13:26 +0300

    rd: redux-form was added to project

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -4,12 +4,14 @@ import dialogsReducer from "./dialogsReducer";
 import userReducer from "./userReducer"
 import authReducer from "./auth-reducer"
 import thunkMiddleware from "redux-thunk"
+import {reducer  as  formReducer} from "redux-form"
 
 let reducersBunch = combineReducers({
     postsPage: profileReducer,
 	dialogsPage: dialogsReducer,
 	userPage: userReducer,
-	auth: authReducer
+	auth: authReducer,
+	form: formReducer,
 })
 
 let store = createStore(reducersBunch, applyMiddleware(thunkMiddleware));
